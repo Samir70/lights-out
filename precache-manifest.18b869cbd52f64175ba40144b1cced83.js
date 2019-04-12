b5148d57771b1adeb6ed1a8d2081cb9ba4d572ae
Samir70home <sbetmouni@talktalk.net>
2019-04-12 13:48:33 +0100

    Updates

---
+++ precache-manifest.18b869cbd52f64175ba40144b1cced83.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1bd7e6b94b5d35bfdd5c52dc34d5d1b",
+    "url": "/lights-out/static/media/Lights-Out.a1bd7e6b.png"
+  },
+  {
+    "revision": "b7f659b28a18816eed64880e73233088",
+    "url": "/lights-out/static/media/Lights-Out-M-before.b7f659b2.png"
+  },
+  {
+    "revision": "547a9a26a861d35c3cc6a877fbb25a93",
+    "url": "/lights-out/static/media/Lights-Out-M-after.547a9a26.png"
+  },
+  {
+    "revision": "d6b7daafe64bb82a9237",
+    "url": "/lights-out/static/js/runtime~main.69e4817a.js"
+  },
+  {
+    "revision": "d55cf5d7641130842afd",
+    "url": "/lights-out/static/js/main.125f0027.chunk.js"
+  },
+  {
+    "revision": "edaac34947ed3fc9733d",
+    "url": "/lights-out/static/js/2.e3c694ef.chunk.js"
+  },
+  {
+    "revision": "d55cf5d7641130842afd",
+    "url": "/lights-out/static/css/main.425bc090.chunk.css"
+  },
+  {
+    "revision": "8af72c33b763818faf8f87090f60b9c6",
+    "url": "/lights-out/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/lights-out/precache-manifest.32e53d77ae54f3808d811e164a32c173.js"
+  "/lights-out/precache-manifest.18b869cbd52f64175ba40144b1cced83.js"
 );
 
 workbox.clientsClaim();
